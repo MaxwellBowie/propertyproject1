171fdda27699ebfdf7f67cc2ad02acfabb5be2d3
LovedeepSaini <saini.lovedeep.kaur@gmail.com>
2024-02-05 12:35:09 +0000

    cypress testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('passes', () => {
+    cy.visit('http://localhost:3001/properties')
+  })
+})
